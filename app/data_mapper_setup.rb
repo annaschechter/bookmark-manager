1b3fb737b1ab8184ea82b6df8480db270528c38d
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-10-09 18:41:57 +0100

    Updated production variable

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,10 +1,9 @@
 env = ENV["RACK_ENV"] || "development"
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
 
 require_relative 'models/link'
 require_relative 'models/tag'
 require_relative 'models/user'
 
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
-
+DataMapper.auto_upgrade!
 DataMapper.finalize
